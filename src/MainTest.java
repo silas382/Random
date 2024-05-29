962f8483886f461f0613a390fc1426160109a16f
Silas Wu <silaswu@Silass-MacBook-Air.local>
2024-05-29 15:20:26 -0500

    changed the print statement

--- src/MainTest.java
+++ src/MainTest.java
@@ -4,7 +4,7 @@ public class MainTest {
     public static void main(String[] args) {
 
         System.out.println("Just random code stuff");
-        System.out.println("LOLLLL");
+        System.out.println("LOL");
 
     }
 }
